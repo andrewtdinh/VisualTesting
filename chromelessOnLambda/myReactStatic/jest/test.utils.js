ed291400a54670ed12843fc03a0fe7655601d53f
Andrew Dinh <adinh@sightmachine.com>
2019-08-30 11:28:21 -0700

    Add a test utility file

---
+++ chromelessOnLambda/myReactStatic/jest/test.utils.js
@@ -0,0 +1,14 @@
+import { Chromeless } from "chromeless";
+
+export const setup = () => {
+  jest.setTimeout(10000);
+  return new Chromeless(global.config.chromeless);
+};
+
+export const teardown = async chromeless => {
+  try {
+    await chromeless.end();
+  } catch (err) {
+    console.log(err);
+  }
+};
