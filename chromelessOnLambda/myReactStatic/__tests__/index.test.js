c0d4ec21df33ebe90d482c836be60c0ef247b90c
Andrew Dinh <adinh@sightmachine.com>
2019-08-30 11:58:35 -0700

    Add simple first script

---
+++ chromelessOnLambda/myReactStatic/__tests__/index.test.js
@@ -0,0 +1,10 @@
+beforeAll(() => { chromeless = setup(); });
+afterAll(async () => { await teardown(chromeless); });
+
+test("+++ home renders correctly", async () => {
+  const html = await chromeless
+    .goto(global.config.baseUrl)
+    .wait(".content")
+    .evaluate(() => document.querySelector(".content").innerHTML);
+  expect(html).toMatchSnapshot();
+});
